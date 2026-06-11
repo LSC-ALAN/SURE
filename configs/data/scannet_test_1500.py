c415703e0d2ee61c8ad4199d132f79c784ef323a
LSC-ALAN <lisicheng0101@gmail.com>
2026-06-11 15:13:30 +0800

    Update scannet_test_1500.py

--- configs/data/scannet_test_1500.py
+++ configs/data/scannet_test_1500.py
@@ -3,7 +3,7 @@ from configs.data.base import cfg
 TEST_BASE_PATH = "assets/scannet_test_1500"
 
 cfg.DATASET.TEST_DATA_SOURCE = "ScanNet"
-cfg.DATASET.TEST_DATA_ROOT = "/usr1/home/s124mdg51_07/accelerated_features/content/ScanNet1500/scannet_test_1500"
+cfg.DATASET.TEST_DATA_ROOT = "xxx/content/ScanNet1500/scannet_test_1500"
 cfg.DATASET.TEST_NPZ_ROOT = f"{TEST_BASE_PATH}"
 cfg.DATASET.TEST_LIST_PATH = f"{TEST_BASE_PATH}/scannet_test.txt"
 cfg.DATASET.TEST_INTRINSIC_PATH = f"{TEST_BASE_PATH}/intrinsics.npz"
